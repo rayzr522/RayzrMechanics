442c0a25313460551652d68767bad0e0604156ac
Rayzr522 <rayzr522@gmail.com>
2017-02-10 10:02:34 -0500

    Project 'RayzrMechanics' generated
    Generated using CNLPluginArchetype and custom CLI project generator tool by Rayzr

---
+++ README.md
@@ -0,0 +1,4 @@
+# RayzrMechanics
+Various random features and mechanics added to the game.
+
+*Note: This README is a pre-generated stub*
---
+++ src/main/java/com/rayzr522/rayzrmechanics/RayzrMechanics.java
@@ -0,0 +1,15 @@
+package com.rayzr522.rayzrmechanics;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Rayzr
+ */
+public class RayzrMechanics extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
